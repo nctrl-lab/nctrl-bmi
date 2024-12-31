9d2c0cdcaf6b8955323bf80fdd13bb558290937c
Dohoung Kim <lapis42@gmail.com>
2024-12-31 15:46:25 +0900

    Fix logger bin_size unit (ms -> s)

--- nctrl/gui.py
+++ nctrl/gui.py
@@ -165,7 +165,7 @@ class NCtrlGUI(QWidget):
                     unit_id = int(self.unit_selector.selectedItems()[0].text())
                     self.nctrl.bmi.set_binner(bin_size=self.bin_size, B_bins=self.B_bins)
                     self.nctrl.set_decoder(decoder=self.decoder, unit_id=unit_id, nspike=self.nspike)
-                    logger.info(f"Fr BMI: bin size {self.bin_size} ms, Bin number {self.B_bins}")
+                    logger.info(f"Fr BMI: bin size {self.bin_size} s, Bin number {self.B_bins}")
                     logger.info(f"Unit ID: {unit_id}, threshold {self.nspike_btn.value()}")
                     logger.info(f"Laser duration: {self.laser_duration} ms")
                 elif self.decoder == 'spikes':
