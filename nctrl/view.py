457e8c7a9b1f50addd140f6e4c069d2a74dfc722
Dohoung Kim <lapis42@gmail.com>
2025-01-08 16:32:10 +0900

    update

--- nctrl/view.py
+++ nctrl/view.py
@@ -24,7 +24,7 @@ class LineView(scene.SceneCanvas):
         self._setup_axes()
         self.freeze()
 
-    def _setup_axes(self, axis_color=(0,1,1,0.8)):
+    def _setup_axes(self, axis_color=(1, 1, 1, 0.8)):
         axis_props = {
             'text_color': axis_color,
             'axis_color': axis_color,
@@ -34,12 +34,13 @@ class LineView(scene.SceneCanvas):
         self.xaxis = scene.AxisWidget(orientation='bottom', **axis_props)
         self.yaxis = scene.AxisWidget(orientation='left', **axis_props)
         
+        # Configure axes
         self.xaxis.stretch = (0.9, 0.1)
         self.yaxis.stretch = (0.1, 0.9)
         
+        # Add to grid and link views
         self.grid.add_widget(self.xaxis, row=1, col=1)
         self.grid.add_widget(self.yaxis, row=0, col=0)
-        
         self.xaxis.link_view(self.view)
         self.yaxis.link_view(self.view)
     
@@ -52,16 +53,10 @@ class LineView(scene.SceneCanvas):
             List of position arrays for each line
         """
         self.clear()
-        
         n_colors = len(self.line_colors)
-        
         for i, pos in enumerate(positions):
-            line = scene.visuals.Line(
-                pos=pos,
-                color=self.line_colors[i % n_colors],
-                width=2
-            )
-            self.view.add(line)
+            color = self.line_colors[i % n_colors]
+            line = scene.visuals.Line(pos=pos, color=color, width=2, parent=self.view.scene)
             self.lines.append(line)
             
         self.view.camera.set_range()
@@ -72,47 +67,43 @@ class LineView(scene.SceneCanvas):
             line.parent = None
         self.lines.clear()
 
-# class fr_view(line_view):
-#     def __init__(self, fs=25e3):
-#         super().__init__()
+class FrView(LineView):
+    def __init__(self, fs=25e3):
+        super().__init__()
 
-#         self.unfreeze()
+        self._fs = fs
+        self._time_tick = 1
+        self._spike_time = np.array([])
 
-#         self._fs = fs
-#         self._time_tick = 1
-#         self._spike_time = np.array([])
-
-#     def add_data(self, spk_time=None):
-#         self._spike_time = np.append(self._spike_time, spk_time)
-#         self._draw()
+    def add_data(self, spk_time=None):
+        self._spike_time = np.append(self._spike_time, spk_time)
+        self._draw()
     
-#     def set_data(self, spk_times=None):
-#         self._spike_time = spk_times
-#         self._draw()
+    def set_data(self, spk_times=None):
+        self._spike_time = spk_times
+        self._draw()
     
-#     def _convolve(self):
-#         times = self._spike_time / int(self._fs * self._time_tick)
-#         print(times)
-#         counts = np.bincount(times.astype(int)) * int(1/self._time_tick)
-#         kernel = signal.gaussian(20, std=5) # should be half gaussian
-#         kernel = kernel[10:]
-#         kernel /= kernel.sum()
-#         fr =  signal.convolve(counts, kernel, mode='same')
-#         return fr
+    def _convolve(self):
+        times = (self._spike_time / int(self._fs * self._time_tick)).astype(int)
+        counts = np.bincount(times) * int(1/self._time_tick)
+        kernel = signal.gaussian(20, std=5)
+        kernel /= kernel.sum()
+        fr =  signal.convolve(counts, kernel, mode='valid')
+        return fr
     
-#     def _draw(self):
-#         poses = []
-#         colors = []
+    def _draw(self):
+        poses = []
+        colors = []
 
-#         rate = self._convolve()
-#         x = np.arange(1, rate.shape[0] + 1)
-#         y = rate
-#         color = np.hstack((palette[1], 1))
-#         pos = np.column_stack((x, y))
-#         poses.append(pos)
-#         colors.append(color)
+        rate = self._convolve()
+        x = np.arange(1, rate.shape[0] + 1)
+        y = rate
+        color = np.hstack((palette[1], 1))
+        pos = np.column_stack((x, y))
+        poses.append(pos)
+        colors.append(color)
 
-#         super().set_data(poses, colors)
+        super().set_data(poses, colors)
 
 if __name__ == '__main__':
     from PyQt5.QtWidgets import QApplication
